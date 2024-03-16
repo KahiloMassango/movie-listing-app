285d7c4e5ebf9dbc3043ac8adbc934d7cfb43364
Kahilo Massango <kahilo42@gmail.com>
2024-03-16 09:15:12 +0100

    minor changes

--- app/src/main/java/com/example/movielistapp/data/local/dao/AppDatabase.kt
+++ app/src/main/java/com/example/movielistapp/data/local/dao/AppDatabase.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.movielistapp.data.network.model.MovieEntity
+import com.example.movielistapp.data.local.entities.MovieEntity
 
-@Database([MovieEntity::class], version = 1, exportSchema = false)
+@Database([MovieEntity::class], version = 2, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
 
     abstract fun movieDao(): MovieDao
